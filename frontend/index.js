2471af339dac714c10b79bbbc2abdd399fc67f7a
Ayla Nelson <anelson@sonos.com>
2025-08-06 18:23:23 -0700

    research setup TODO stx

--- frontend/index.js
+++ frontend/index.js
@@ -2,6 +2,9 @@ import { RecursiveCharacterTextSplitter } from 'langchain/text_splitter'
 try {
     const result = await fetch('info.txt')
     const text = await result.text()
+    //COMMENTS: Limitations on inputs
+
+
 } catch (err){
     console.log(err)
 }
